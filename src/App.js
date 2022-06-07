40bfaa8ebbb561e34b6f15b32b9db46537097d0f
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-06-07 18:59:26 -0500

    has home component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import UserCard from './components/UserCard';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from './components/Home';
 
 
 
 function App() {
   return (
-    <>
-      <Route path="/">
-        <UserCard />
+    <Switch>
+    
+
+      <Route exact path="/">
+        <Home />
       </Route>
+
       <Route path="/users">
         <UserCard />
       </Route>
-      </>
+
+      </Switch>
 
   );
 }
