5b0993ac8bde5238754a3ed9caecf5ab08c12604
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-06-08 14:58:47 -0500

    edit and delete button are moved to bottom of card

--- src/components/DisplayProjectCard.js
+++ src/components/DisplayProjectCard.js
@@ -3,10 +3,8 @@ import React from "react";
 export default function DisplayProjectCard({project, handleDelete, handleClickEdit}){
     return(
     <div>
-        <h3>{project.name}</h3>
-        <button onClick={handleClickEdit} >{`Edit This Project`} ^</button>
-
-<button onClick={()=>handleDelete(project.id)} >Delete This Project^</button>
+ 
+            <h3>{project.name}</h3>
             <ul>
                 <li>
                     Created by: {project.author}
@@ -15,6 +13,11 @@ export default function DisplayProjectCard({project, handleDelete, handleClickEd
                     Project Status: {project.status}
                 </li>
             </ul>
+                   
+
+            <button onClick={handleClickEdit} >{`Edit This Project`} ^</button>
+
+            <button onClick={()=>handleDelete(project.id)} >Delete This Project^</button>
     </div>
     )
 }
