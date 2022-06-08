dca9d3cfa05127e81f9ea292966f80e7ce9931ba
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-06-08 13:14:55 -0500

    delete button works with refresh

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,11 +2,17 @@ import React from "react";
 
 export default function ProjectCard({project}){
 
+
+    function handleDelete(id){
+    console.log('I want to delete', id)
+    fetch(`http://localhost:4000/Projects/${id}`, {method: "DELETE"})
+    }
+
     return(
         <div>
             <h3>{project.name}</h3>
             <button>Edit This Project^</button>
-            <button>Delete This Project^</button>
+            <button onClick={()=>handleDelete(project.id)} >Delete This Project^</button>
             <ul>
                 <li>
                     Created by: {project.author}
