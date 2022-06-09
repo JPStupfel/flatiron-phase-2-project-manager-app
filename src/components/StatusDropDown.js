c79e392ac69d2a00b55a488e2b2368ca0ffbe6f9
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-06-09 11:53:20 -0500

    set status button says status on it

--- src/components/StatusDropDown.js
+++ src/components/StatusDropDown.js
@@ -2,11 +2,11 @@ import React from "react"
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
 
+//pass this function a set helper function to update state and the current state as a prop so it can update the title shown on the button
 
 export default function StatusDropDown({onSetStatus, buttonTitle}){
-    console.log(buttonTitle.status)
     return(
-    <DropdownButton id="dropdown-basic-button" title={buttonTitle.status ? buttonTitle.status : 'select'}>
+    <DropdownButton id="dropdown-basic-button" title={buttonTitle.status ? `Status: ${buttonTitle.status}` : 'Status: Choose a Status!'}>
 
         <Dropdown.Item
          href="#/action-1"
