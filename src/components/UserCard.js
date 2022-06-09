eb2c91da2288b49b5688e04c69e4d2e3a66265dc
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-06-09 16:03:10 -0500

    user cards use bootstrap style

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -4,6 +4,8 @@ import Button from 'react-bootstrap/Button';
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
 import  '../App.css';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 
 
@@ -22,28 +24,15 @@ function onDelete(event){
   return (
   <div className="App">
         
-    <Card>
-
-      <Card.Img variant="top" src={user.image} />
-
-      <Card.Body>
-        <Card.Title>{user.name }</Card.Title>
-        <Card.Text>
-        Projects Created: {user["Projects Created"]}
-        </Card.Text>
-      </Card.Body>
-
-      <DropdownButton id="dropdown-basic-button" title="Delete User">
-        <Dropdown.Item onClick={onDelete} href="#/action-1">{`Delete ${user.name}?`}</Dropdown.Item>
-       
-      </DropdownButton>
-
-
-      <Card.Footer>
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </Card.Footer>
-
-    </Card>
+        <Card>
+            <Card.Img variant="top" src={user.image}/>
+            <Card.Body>
+              <Card.Title>{user.name}</Card.Title>
+              <Card.Text>
+                {`${user.name} has created ${user["Projects Created"]} projects.`}
+              </Card.Text>
+            </Card.Body>
+          </Card>
 
   </div>
   );
