d7692dac3d5ce4b9c45c3bff515306a2faba7d3b
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-06-11 19:49:34 -0500

    log in form has inputs

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,10 +1,79 @@
-import React from "react";
+import React, {useState} from "react";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import AddUser from "./AddUser";
 
-export default function Login(){
+export default function Login({onAddloginUser, onCancelAddUser}){
 
-    return(
-        <form>
-            hello fish 
-        </form>
+
+    const [loginUser, setLoginUser] = useState({'name':'','password': '' })
+
+    function handleChangeInput(event){
+        const updatedUser = {...loginUser, [event.target.name]: event.target.value}
+        setLoginUser(updatedUser)
+
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+        console.log('will log in',loginUser)
+
+    }
+
+
+    return(<>
+    <Form
+    onSubmit={handleSubmit} 
+    >
+
+        <Form.Group 
+        className="mb-3" 
+        
+        >
+            <Form.Label>User Name</Form.Label>
+
+            <Form.Control 
+            name={'name'}  
+            type="text" 
+            placeholder="Enter User Name" 
+            value={loginUser.name}
+            onChange={handleChangeInput}/>
+           
+
+            <Form.Text className="text-muted">
+            Give your User a memorable, but informative name!
+            </Form.Text>
+        </Form.Group>
+
+
+        <Form.Group    
+        className="mb-3" 
+        >
+            <Form.Label>Password</Form.Label>
+            <Form.Control 
+            name='password' 
+            type="text" 
+            placeholder="Provide password"
+            onChange={handleChangeInput}
+            value={loginUser.password}
+            />
+        
+        </Form.Group>
+
+
+
+        <Button 
+        variant="primary" type="submit">
+            Submit
+        </Button>
+        
+    </Form>
+
+    <Button
+    onClick={onCancelAddUser}
+    variant="primary" type="text">
+        Cancel
+    </Button>
+    </>
     )
 }
